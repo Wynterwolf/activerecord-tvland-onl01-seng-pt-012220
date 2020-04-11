da9234f9a687873299a763eb109a33a871ded493
Samantha Jordan <shinyguns@gmail.com>
2020-04-11 18:37:46 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
+  #actor has many characters
+  has_many :characters
   
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{characters.show.name}"
+    end.join
+  end
 end
